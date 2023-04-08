899c9c052ecc99f5a3814659d63b4dac14fb2824
zockq1 <zockq1@gmail.com>
2023-04-08 21:36:59 +0900

    refactor: 필요없는 코드 제거

--- src/pages/Topic.tsx
+++ src/pages/Topic.tsx
@@ -1,22 +1,17 @@
-import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import styled from "styled-components";
 import ChapterList from "../components/ChapterList";
 import TopicList from "../components/TopicList";
-import { RootState } from "../store/store";
 
 const InfosLayout = styled.div`
   display: flex;
 `;
 
 function Topic() {
-  const { currentChapterNumber } = useSelector(
-    (state: RootState) => state.chapter
-  );
   return (
     <InfosLayout>
       <ChapterList />
-      {currentChapterNumber ? <TopicList /> : null}
+      <TopicList />
       <Outlet />
     </InfosLayout>
   );
