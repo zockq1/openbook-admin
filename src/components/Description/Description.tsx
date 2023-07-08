b8b8f25c299e91abf907743702bdd5586ccdb802
zockq1 <zockq1@gmail.com>
2023-07-08 20:22:00 +0900

    refactor: 보기 삭제 컴포넌트 분리

--- src/components/Description/Description.tsx
+++ src/components/Description/Description.tsx
@@ -1,20 +1,30 @@
 import { List, Space, Input, Button } from "antd";
-import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import { EditOutlined } from "@ant-design/icons";
 import { useState } from "react";
 import { DescriptionModel } from "../../types/descriptionType";
 import DescriptionModal from "./DescriptionModal";
+import { useUpdateDescriptionMutation } from "../../store/api/descriptionApi";
+import { mutationErrorNotification } from "../../services/errorNotification";
+import DeleteDescriptionButton from "./DeleteDescriptionButton";
 
 interface DescriptionProps {
   data: DescriptionModel;
-  onEdit: (id: number, content: string) => void;
-  onDelete: (id: number) => void;
 }
 
-function Description({ data, onEdit, onDelete }: DescriptionProps) {
+function Description({ data }: DescriptionProps) {
+  const [updateDescription] = useUpdateDescriptionMutation();
   const [isEditing, setIsEditing] = useState(false);
   const [editContent, setEditContent] = useState(data.content);
 
-  const handleEdit = () => {
+  const handleEdit = async (id: number, content: string) => {
+    try {
+      await updateDescription({ descriptionId: id, content }).unwrap();
+    } catch (error) {
+      mutationErrorNotification(error);
+    }
+  };
+
+  const onEdit = () => {
     setEditContent(data.content);
     setIsEditing(true);
   };
@@ -24,7 +34,7 @@ function Description({ data, onEdit, onDelete }: DescriptionProps) {
   };
 
   const handleSave = () => {
-    onEdit(data.id, editContent);
+    handleEdit(data.id, editContent);
     setIsEditing(false);
   };
 
@@ -36,14 +46,14 @@ function Description({ data, onEdit, onDelete }: DescriptionProps) {
     <List.Item
       actions={[
         <Space>
-          {!isEditing && <EditOutlined onClick={handleEdit} />}
+          {!isEditing && <EditOutlined onClick={onEdit} />}
           {isEditing && (
             <>
               <Button onClick={handleSave}>저장</Button>
               <Button onClick={handleCancel}>취소</Button>
             </>
           )}
-          <DeleteOutlined onClick={() => onDelete(data.id)} />
+          <DeleteDescriptionButton descriptionId={data.id} />
         </Space>,
       ]}
     >
--- src/components/Description/DescriptionList.tsx
+++ src/components/Description/DescriptionList.tsx
@@ -1,59 +1,23 @@
 import { useParams } from "react-router-dom";
-import { List, Modal } from "antd";
-import {
-  useDeleteDescriptionMutation,
-  useGetDescriptionsQuery,
-  useUpdateDescriptionMutation,
-} from "../../store/api/descriptionApi";
+import { List } from "antd";
+import { useGetDescriptionsQuery } from "../../store/api/descriptionApi";
 import Description from "./Description";
-import {
-  mutationErrorNotification,
-  queryErrorNotification,
-} from "../../services/errorNotification";
+import { queryErrorNotification } from "../../services/errorNotification";
 import { useEffect } from "react";
 
 function DescriptionList() {
   const { title } = useParams();
   const { data: descriptionList, error: descriptionListError } =
     useGetDescriptionsQuery(String(title));
-  const [updateDescription] = useUpdateDescriptionMutation();
-  const [deleteDescription] = useDeleteDescriptionMutation();
 
   useEffect(() => {
     queryErrorNotification(descriptionListError, "보기 목록");
   }, [descriptionListError]);
 
-  const handleEdit = async (id: number, content: string) => {
-    try {
-      await updateDescription({ descriptionId: id, content }).unwrap();
-    } catch (error) {
-      mutationErrorNotification(error);
-    }
-  };
-
-  const handleDelete = async (id: number) => {
-    Modal.confirm({
-      title: "주의",
-      content: "정말 이 보기를 삭제하시겠습니까?",
-      okText: "예",
-      okType: "danger",
-      cancelText: "아니오",
-      onOk: async () => {
-        try {
-          await deleteDescription(id).unwrap();
-        } catch (error) {
-          mutationErrorNotification(error);
-        }
-      },
-    });
-  };
-
   return (
     <List
       dataSource={descriptionList}
-      renderItem={(item) => (
-        <Description data={item} onEdit={handleEdit} onDelete={handleDelete} />
-      )}
+      renderItem={(item) => <Description data={item} />}
     />
   );
 }
